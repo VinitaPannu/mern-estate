6199bc68e93d134148b9cbdfdee9c39a3efd9976
Vinita <102506425+VinitaPannu@users.noreply.github.com>
2025-01-20 22:58:31 +0530

    sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -9,9 +10,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => { console.log(err); });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3300, () => {
   console.log('Server is running on port 3300!');
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
