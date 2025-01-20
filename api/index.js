754a6e61fae02ea086a0269bb041037bb72bc813
Vinita <102506425+VinitaPannu@users.noreply.github.com>
2025-01-20 19:35:35 +0530

    test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -12,3 +13,5 @@ const app = express();
 app.listen(3300, () => {
   console.log('Server is running on port 3300!');
 });
+
+app.use("/api/user", userRoutes);
